fd90fc14efd7b8acb7cb51c692dc62d65dc2e44d
Christian Martinez <martchfr@iu.edu>
2025-09-06 19:17:08 +0000

    Added functions to the correct submission place

--- apputil.py
+++ apputil.py
@@ -1,3 +1,21 @@
 
+def palindrome (word):
+    clean = word.replace(",","").replace(".","").replace(" ","").lower()
+    if clean == clean[::-1]:
+        return True
+    else: 
+        return False
 
-# add code below ...
+palindrome(word)
+
+def parenthesis_balance(string):
+    count = 0
+    for char in string:
+        if char == "(":
+            count += 1
+        elif char == ")":
+            count -= 1
+            if count < 0:
+                return False
+
+parenthesis_balance(string)
